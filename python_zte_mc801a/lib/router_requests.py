a2b9b097f7875d2e4bd9c7a9ee6a294fc586715f
projectyelen <88148238+sandriser@users.noreply.github.com>
2024-11-28 19:56:48 +0100

    Update router_requests.py

--- python_zte_mc801a/lib/router_requests.py
+++ python_zte_mc801a/lib/router_requests.py
@@ -57,6 +57,7 @@ def get_auth_cookies(router_ip: str, user_password: str) -> dict:
         f"https://{router_ip}/goform/goform_get_cmd_process?isTest=false&cmd=LD",
         cookies={"stok": ""},
         headers={"referer": f"https://{router_ip}/"},
+        verify=False,
     )
 
     # The password is hashed twice
@@ -73,6 +74,7 @@ def get_auth_cookies(router_ip: str, user_password: str) -> dict:
         f"https://{router_ip}/goform/goform_set_cmd_process?isTest=false&goformId=LOGIN&password={pwd}",
         cookies={"stok": ""},
         headers={"referer": f"https://{router_ip}/"},
+        verify=False,
     )
 
     if (not "result" in r_login.json().keys()) or (r_login.json()["result"] != "0"):
@@ -96,6 +98,7 @@ def get_signal_data(router_ip: str, auth_cookies: dict) -> dict:
         f'https://{router_ip}/goform/goform_get_cmd_process?isTest=false&cmd={",".join(ALL_DATA_FIELDS)}&multi_data=1',
         cookies=auth_cookies,
         headers={f"referer": f"https://{router_ip}/"},
+        verify=False,
     )
 
     return r_data.json()
@@ -116,6 +119,7 @@ def get_latest_sms_messages(router_ip, auth_cookies, n=3) -> list:
         f"https://{router_ip}/goform/goform_get_cmd_process?isTest=false&cmd=sms_data_total&page=0&data_per_page=500&mem_store=1&tags=10&order_by=order+by+id+desc",
         cookies=auth_cookies,
         headers={f"referer": f"https://{router_ip}/"},
+        verify=False,
     )
 
     json_data = r_data.json()
@@ -165,6 +169,7 @@ def set_5g_band(
         data=request_data,
         cookies=auth_cookies,
         headers=headers,
+        verify=False,
     )
 
     if (
