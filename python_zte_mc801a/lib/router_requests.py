9e36e0944d9ef4f8712a4daa99f48ca42f134a05
projectyelen <88148238+sandriser@users.noreply.github.com>
2024-11-28 19:54:02 +0100

    Update router_requests.py

--- python_zte_mc801a/lib/router_requests.py
+++ python_zte_mc801a/lib/router_requests.py
@@ -26,6 +26,8 @@ class CustomHttpAdapter (requests.adapters.HTTPAdapter):
 def get_legacy_session():
     ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
     session = requests.session()
     session.mount('https://', CustomHttpAdapter(ctx))
     return session
